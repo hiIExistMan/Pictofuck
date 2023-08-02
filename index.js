1fd91bb5765ad53048846c2075eddb141596629c
hiIExistMan <codymotes2010@gmail.com>
2023-08-01 20:50:46 -0400

    removed unnessecary console.log calls and finally fixed save and load

--- index.js
+++ index.js
@@ -5,39 +5,46 @@ let squareSize = 25;
 let consoleText = "";
 let cursorColor;
 let running = false;
+
 function initProgram() {
-    for(let i = 0; i < programHeight; i++) {
+    for (let i = 0; i < programHeight; i++) {
         program[i] = [];
-        for(let j = 0; j < programWidth; j++) {
-            program[i][j] = color(255,255,255);
+        for (let j = 0; j < programWidth; j++) {
+            program[i][j] = color(255, 255, 255);
         }
     }
 }
 
 function saveProgram() {
-    let output = {program:[]};
-    for(let x = 0; x < programWidth; x++) {
+    let output = {
+        program: []
+    };
+    for (let x = 0; x < programWidth; x++) {
         output.program[x] = [];
-        for(let y = 0; y < programHeight; y++) {
-            output.program[x].push(color2int(program[y][x]))
+        for (let y = 0; y < programHeight; y++) {
+            let c = program[y][x];
+            let r = red(c);
+            let g = green(c);
+            let b = blue(c);
+            output.program[x].push([r, g, b])
         }
     }
 
-    saveJSON(output,"program.json")
+    saveJSON(output, "program.json")
 }
 
 function load(json) {
-    console.log(json);
     let data = json.data.program;
     let prog = [];
-    for(let y = 0; y < data.length; y++) {
-        prog[y] = []
+    for (let x = 0; x < data[0].length; x++) {
 
-        for(let x = 0; x < data[0].length; x++) {
-            prog[y][x] = int2color(data[x][y])
+        prog[x] = []
+        for (let y = 0; y < data.length; y++) {
+            let d = data[y][x];
+            prog[x][y] =
+                color(d[0], d[1], d[2])
         }
     }
-    console.log(data,prog);
     program = prog;
 }
 
@@ -58,39 +65,41 @@ function stop() {
 }
 
 function setup() {
-    let canv = createCanvas(squareSize*programWidth,squareSize*programHeight);
+    let canv = createCanvas(squareSize * programWidth, squareSize * programHeight);
     canv.parent(select("#screen"));
     canv.mousePressed(mp)
     initProgram();
-    cursorColor = color(255,255,255)
+    cursorColor = color(255, 255, 255)
     frameRate(2);
     createFileInput(load).class("programInput").parent(select(".file-wrapper"))
-    select(".fpsSlider").changed(e => {frameRate(Number(e.target.value));console.log(e.target.value)})
+    select(".fpsSlider").changed(e => {
+        frameRate(Number(e.target.value));
+    })
     initInterpreter();
 }
 
 
 function draw() {
-    for(let x = 0; x < programWidth; x++) {
-        for(let y = 0; y < programHeight; y++) {
+    for (let x = 0; x < programWidth; x++) {
+        for (let y = 0; y < programHeight; y++) {
             fill(program[y][x])
             strokeWeight(2);
-            if(comX == x & comY == y) {
+            if (comX == x & comY == y) {
                 strokeWeight(4);
             }
             stroke(0);
-            rect(x*squareSize,y*squareSize,squareSize);
+            rect(x * squareSize, y * squareSize, squareSize);
         }
     }
-    if(running) {
+    if (running) {
         step();
         select("textarea").value(consoleText);
     }
 }
 
 function mp() {
-    if(floor(mouseY/squareSize) > programHeight || floor(mouseX/squareSize) > programWidth) return
-    program[floor(mouseY/squareSize)][floor(mouseX/squareSize)] = cursorColor;
+    if (floor(mouseY / squareSize) > programHeight || floor(mouseX / squareSize) > programWidth) return
+    program[floor(mouseY / squareSize)][floor(mouseX / squareSize)] = cursorColor;
 
 
 }
@@ -101,5 +110,4 @@ function changeCursorColor(e) {
     var targ = e.target || e.srcElement || e;
     if (targ.nodeType == 3) targ = targ.parentNode; // defeat Safari bug
     cursorColor = color(targ.style.backgroundColor);
-    console.log(targ.style.backgroundColor)
 }
